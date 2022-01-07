3f1d71213904bbe885e212c6e6a4c291935abaa6
sunpengyu.soniav2 <sunpengyu.soniav2@bytedance.com>
2022-01-07 10:28:23 +0800

    初步计划
    Change-Id: I183e35ed07121e85493e5da6cf68ed9d0cdd6fbe

---
+++ RecommendService/src/main/java/com/bubble/Model/Project.java
@@ -0,0 +1,9 @@
+package com.bubble.Model;
+
+/**
+ * @Author: sunpengyu.sonia
+ * @Date: 2022/1/6 9:46 PM
+ * @Desc: 定义物品画像内容
+ */
+public class Project {
+}
---
+++ RecommendService/src/main/java/com/bubble/Model/UserPurchaseRecord.java
@@ -0,0 +1,9 @@
+package com.bubble.Model;
+
+/**
+ * @Author: sunpengyu.sonia
+ * @Date: 2022/1/6 9:48 PM
+ * @Desc:
+ */
+public class UserPurchaseRecord {
+}
