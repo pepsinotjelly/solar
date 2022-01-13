385038adae79cf763eb9e638541726fa073cb2e6
pepsinotjelly <1637597600@qq.com>
2022-01-14 00:26:50 +0800

    python increase

---
+++ recommend_web/config.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：solar 
+@File    ：config.py.py
+@Author  ：Sonia.Suen
+@Date    ：2022/1/14 12:17 AM 
+"""
---
+++ recommend_web/templates/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：solar 
+@File    ：__init__.py.py
+@Author  ：Sonia.Suen
+@Date    ：2022/1/14 12:16 AM 
+"""
