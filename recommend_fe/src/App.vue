ca91b4b5f29572a8fb9ca63bbd1421e3ff98d953
sunpengyu.soniav2 <sunpengyu.soniav2@bytedance.com>
2022-01-18 14:16:29 +0800

    route
    Change-Id: Ifbf1960fc9f3776476ca5c15bfbe1dbe648d728e

--- recommend_fe/src/App.vue
+++ recommend_fe/src/App.vue
@@ -2,34 +2,41 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+<!--    <h2>Essential Links</h2>-->
+<!--    <ul>-->
+<!--      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>-->
+<!--      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>-->
+<!--      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>-->
+<!--      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>-->
+<!--    </ul>-->
+<!--    <h2>Ecosystem</h2>-->
+<!--    <ul>-->
+<!--      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>-->
+<!--      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>-->
+<!--      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>-->
+<!--      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>-->
+<!--    </ul>-->
+<!--    <secondcomponent></secondcomponent>-->
     <testcomponent></testcomponent>
+    <ul>
+      <li><router-link to="/first">click me to page 1</router-link></li>
+      <li><router-link to="/second">click me to page 2</router-link></li>
+    </ul>
+    <router-view class="view"></router-view>
   </div>
 </template>
 
 <script>
 import testcomponent from "./components/testcomponent";
+import Secondcomponent from "./components/secondcomponent";
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Hi, Sonia!'
     }
   },
-  components : { testcomponent }
+  components : {Secondcomponent, testcomponent }
 }
 </script>
 
--- recommend_fe/src/main.js
+++ recommend_fe/src/main.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router";
+import VueResource from "vue-resource";
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+Vue.config.debug = true;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const First = {template: '<div><h2>我是第一个页面</h2></div>'}
+import secondcomponent from "./components/secondcomponent";
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/first',
+      component: First
+    },
+    {
+      path: '/second',
+      component: secondcomponent
+    }
+  ]
 })
+
+const app = new Vue({
+  router: router,
+  render: h => h(App)
+}).$mount('#app')
