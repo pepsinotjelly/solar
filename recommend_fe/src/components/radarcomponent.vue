ffe459c62cd718c89d81f6f44aea2d65f28c0225
sunpengyu.soniav2 <sunpengyu.soniav2@bytedance.com>
2022-01-19 20:38:09 +0800

    优化
    Change-Id: I531ab5f8b9846b5c2efcfb51da3391347dfe90d2

--- recommend_fe/src/components/radarcomponent.vue
+++ recommend_fe/src/components/radarcomponent.vue
@@ -13,8 +13,8 @@ export default {
     return {}
   },
   methods: {
-    drawline() {
-      let radarChart = this.$echarts.init(document.getElementById('radarChart'));
+    initRadar() {
+      let radarChart = this.$echarts.init(document.getElementById('radarChart')); // 初始化chart
       let dataMax = [
         {name: '年龄群体', max: '100'},
         {name: '消费口味', max: '100'},
@@ -23,13 +23,43 @@ export default {
         {name: '最后一点', max: '100'}
       ]
       let option = {
+        calculable : true,
         radar: {
           name: {
+            formatter: [`{a|${this.type}}`,
+              '{b|{value}}'].join('\n'),
+            rich: {
+              a: {
+                color: 'rgba(5, 193, 174, 1)',
+                align: 'middle'
+              },
+              b: {
+                color: '#333'
+              },
+            },
             show: true,
             color: 'rgba(255,0,0,0)'
           },
           indicator: dataMax,
-          shape: 'circle'
+          shape: 'circle',
+          splitNumber: 5
+        },
+        title: {
+          text: "你和小仓的相似之处",
+          left: 'center',
+          textStyle: {
+            fontSize: 16,
+            fontWeight: 'bolder',
+          },
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: ["年龄群体", "消费口味", "我说什么好", "爱咋咋吧", "最后一点"]
         },
         series:[
           {
@@ -45,7 +75,7 @@ export default {
             },
             {
               name: '小仓',
-              value: [89, 90, 29, 48, 29]
+              value: [89, 90, 79, 88, 29]
             },
           ],
         },
@@ -55,7 +85,7 @@ export default {
     },
   },
   mounted() {
-    this.drawline()
+    this.initRadar()
   },
 }
 </script>
